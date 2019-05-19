57901e39bda1d20eb3b7ddb19d7f5b179c8e2b54
Neptune Zheng <neptune.zhenge@oocl.com>
2019-05-19 16:16:00 +0800

    add new setup

--- src/main/java/neptune/controller/LoginController.java
+++ src/main/java/neptune/controller/LoginController.java
@@ -1,25 +1,31 @@
 package neptune.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import neptune.domain.User;
 import neptune.po.Hero;
 import neptune.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * Created by ZHENGNE on 8/5/2018.
  */
 @Controller  // for html, use RestController(@Responsebody + @Controller); for jsp, could only use @Controller
+@CrossOrigin
 public class LoginController {
+    protected final Logger logger = LogManager.getLogger(this.getClass());
+
     @Autowired
     private UserService userService;
 
@@ -30,18 +36,36 @@ public class LoginController {
 
     @RequestMapping(value = {"/index"})
     public String loginPage(){
-        userService.verifyUserLogin("aaa","aaa");
         return "loginPage";
     }
 
-    @RequestMapping(value =  "/loginCheck.html")
-    public ModelAndView loginCheck(HttpServletRequest request){
-        return null;
+    @RequestMapping(value =  "/loginCheck", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String loginCheck(@RequestBody Map<?,?> map){
+        if(userService.verifyUserLogin(map.get("userName").toString(),map.get("userPassword").toString())){
+            return "mainPage";
+        }else{
+            return loginPage();
+        }
     }
 
     @RequestMapping(value = "/cHeroes")
     @ResponseBody
     public List<Hero> getHeroes(){
+        logger.info("accept ../cHeroes request");
+        List<Hero> heroes = new ArrayList<>();
+        int i = 0;
+        while (i< 7){
+            Hero hero = new Hero(i*3,"Eva"+i);
+            heroes.add(hero);
+            i++;
+        }
+        return heroes;
+    }
+
+    @RequestMapping(value = "/cHeroes/")
+    @ResponseBody
+    public List<Hero> getHeroe(){
         System.out.println("accept ../cHeroes request");
         List<Hero> heroes = new ArrayList<>();
         int i = 0;
@@ -51,6 +75,22 @@ public class LoginController {
             i++;
         }
         return heroes;
+    }
+
+    @RequestMapping(value = "/cHeroes/{id}")
+    @ResponseBody
+    public Hero getHeroeB(@PathVariable(name = "id") String id){
+        logger.info("accept ../cHeroes request");
+
+        return new Hero(99,"Boss");
+    }
+
+    @RequestMapping(value = "/cHeroes/update", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public Hero updateHeroe(@RequestBody Map<?,?> map){
+        logger.info("accept ../cHeroes/update request: %s",map.get("segment"));
+
+        return new Hero(0,"Init");
     }
 
 }
